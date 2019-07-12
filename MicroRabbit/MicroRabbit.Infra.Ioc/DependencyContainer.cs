5b2301aed05202fdebd39f1946ff65827fad6e0e
Ajit Singh <ajit51081@gmail.com>
2019-07-12 09:43:47 +0530

    partial complete banking microservice

---
+++ MicroRabbit/MicroRabbit.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using MicroRabbit.Infra.Bus;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Data.Context;
+
+namespace MicroRabbit.Infra.Ioc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+            services.AddTransient<IAccountService, AccountService>();
+            services.AddTransient<IAccountRepository,AccountRepository>();
+            services.AddTransient<BankingDbContext>();
+
+        }
+    }
+}
